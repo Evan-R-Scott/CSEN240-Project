61e9fd3e9789476ffe7e796d347290575587e718
Evan Scott <evan.scott@dc.rr.com>
2025-12-03 18:53:57 -0800

    fix naming convention

---
+++ save_weights_local_script.py
@@ -0,0 +1,6 @@
+from tensorflow.keras.applications import EfficientNetB0, ResNet50, DenseNet121
+
+# model = DenseNet121(weights="imagenet", include_top=False)
+model = EfficientNetB0(weights="imagenet", include_top=False)
+# model = ResNet50(weights="imagenet", include_top=False)
+model.save_weights("pretrained_weights/effnetb0_weights.weights.h5")
